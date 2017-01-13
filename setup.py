ef5d72034e9131893e5e0b3acbedc0a92a559623
Ramon Saraiva <ramonsaraiva@gmail.com>
2017-01-13 13:51:55 -0200

    Added setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='jinjaninja',
+      version='0.1',
+      description='Jinja template style enforcement tool',
+      url='https://github.com/ramonsaraiva/jinjaninja',
+      author='Ramon Saraiva',
+      author_email='ramonsaraiva@gmail.com',
+      license='MIT',
+      packages=['jinjaninja'],
+      scripts=['jinjaninja/bin/jinja-ninja'],
+      zip_safe=False)
