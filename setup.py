4694ab09fdcee85f08cdb68a0319d6457d0b8efb
Peter Rosemann <dkdndes@users.noreply.github.com>
2020-03-11 22:02:38 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='jinjaninja',
-      version='0.1.2',
+      version='0.1.3',
       description='Jinja template style enforcement tool',
       url='https://github.com/ramonsaraiva/jinjaninja',
       author='Ramon Saraiva',
