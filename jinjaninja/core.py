9223948dca26a4374e1b985b1ef3a858f4a5b54e
Tharlaw <tharlaw@example.com>
2020-09-08 11:52:49 +0000

    Set file line numbers to begin at 1 not 0

--- jinjaninja/core.py
+++ jinjaninja/core.py
@@ -67,6 +67,6 @@ def validate_template(filename):
 
     with open(filename, "r") as f:
         for i, line in enumerate(f):
-            template_errors += validate_line(filename, line, i)
+            template_errors += validate_line(filename, line, i + 1)
 
     return template_errors
